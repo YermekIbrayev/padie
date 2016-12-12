f3e9271d58f882f807ab2711aabc995cbc76353d
yibrayev <yibrayev@avantetech.com>
2016-12-12 13:40:59 -0500

    serviceHelper after order action added

--- ServiceHelper/app/src/main/java/com/iskhak/servicehelper/ui/EstimatedOrderActivity.java
+++ ServiceHelper/app/src/main/java/com/iskhak/servicehelper/ui/EstimatedOrderActivity.java
@@ -1,6 +1,8 @@
 package com.iskhak.servicehelper.ui;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -33,10 +35,12 @@ public class EstimatedOrderActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         activityComponent().inject(this);
         setContentView(R.layout.activity_estamated_order);
+
         TextView totalEstimated = (TextView)findViewById(R.id.totalEstimated);
         int totalSum = getIntent().getIntExtra("TotalSum", 0);
         totalEstimated.setText("$"+totalSum);
         mContext = this;
+        Timber.d("Created");
     }
 
     public void onCancelButton(View view){
@@ -45,34 +49,53 @@ public class EstimatedOrderActivity extends BaseActivity {
 
     public void onOrderButton(View view){
         if(mSubscribtion!=null && !mSubscribtion.isUnsubscribed()) mSubscribtion.unsubscribe();
-        mSubscribtion = mDataManager.sendOrder(DataHolder.getInstance().getOrder())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<PackageModel>() {
-                    @Override
-                    public void onCompleted() {
-                        Timber.i("got packageModel");
-                        DialogFactory.createSimpleOkErrorDialog(mContext, R.string.thanks_title, R.string.thanks_content).show();
-                    }
+        try {
+            mSubscribtion = mDataManager.sendOrder(DataHolder.getInstance().getOrder())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Observer<PackageModel>() {
+                        @Override
+                        public void onCompleted() {
+                            Timber.i("got packageModel");
+                            showDialog();
+                        }
 
-                    @Override
-                    public void onError(Throwable e) {
-                        e.printStackTrace();
-                    }
+                        @Override
+                        public void onError(Throwable e) {
+                            e.printStackTrace();
+                        }
 
-                    @Override
-                    public void onNext(PackageModel packageModel) {
-                        Timber.i(packageModel.toString());
-                        DataHolder.getInstance().setOrder(packageModel);
-                        Intent intent = new Intent(getApplicationContext(), EstimatedOrderActivity.class);
-                        int totalSum = packageModel.price().intValue();
-                        intent.putExtra("TotalSum", totalSum);
-                        startActivity(intent);
-
-                        
-                    }
-                });
+                        @Override
+                        public void onNext(PackageModel packageModel) {
+                            Timber.i(packageModel.toString());
+                            DataHolder.getInstance().setOrder(packageModel);
+/*                            Intent intent = new Intent(getApplicationContext(), EstimatedOrderActivity.class);
+                            int totalSum = packageModel.price().intValue();
+                            intent.putExtra("TotalSum", totalSum);
+                            startActivity(intent);*/
+                        }
+                    });
+        } catch (Exception e){
+            Timber.e(e, "On order button");
+        }
 /*        Intent intent = new Intent(this, OrderSummaryActivity.class );
         startActivity(intent);*/
+    }
+
+    private void showDialog(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext)
+                .setTitle(getString(R.string.thanks_title))
+                .setMessage(getString(R.string.thanks_content))
+                .setNeutralButton(R.string.dialog_action_ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(mContext, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+        alertDialog.create().show();
+        Timber.d("Dialog showed");
     }
 }
